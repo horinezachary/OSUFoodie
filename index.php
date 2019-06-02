3f7b41b6fd500f8b4566b0896bfe15ccb09611c0
Thomas Korsness <korsnest@flip1.engr.oregonstate.edu>
2019-06-02 16:31:42 -0700

    small fix

--- index.php
+++ index.php
@@ -46,17 +46,7 @@
           </div>
         </nav>
 
-        <?php
-          include 'connectvars.php';
-          include 'header.php';
 
-          $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-          if (!$conn) {
-            die('Could not connect: ' . mysql_error());
-}
-        ?>
-
-    <footer>2019 Website</footer>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
