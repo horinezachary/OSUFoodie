92062112b8bbe77984fc1a80ffe3aa8a3c37b6d2
Zachary Horine <horinez@oregonstate.edu>
2019-06-02 13:37:54 -0700

    add base files from suppliers demo

---
+++ connectvars.php
@@ -0,0 +1,8 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'classmysql.engr.oregonstate.edu');
+  define('DB_USER', 'cs340_horinez');
+  define('DB_PASSWORD', 'BBpF5Cg2LRCuc8F');
+  define('DB_NAME', 'cs340_horinez');
+  define('CON_STRING', 'mysql:host=classmysql.engr.oregonstate.edu;dname=cs340_username');
+?>
---
+++ pages.php
@@ -0,0 +1,9 @@
+<?php
+//$content holds a 2D array with keys being menu names and values being an array with a subtitle, and content
+	$content = array(
+		"Home" => "home.php",
+		"Restaurant" => "restaurant.php",
+		"Account" => "account.php",
+		"About Us" => "aboutUs.php");
+
+?>
