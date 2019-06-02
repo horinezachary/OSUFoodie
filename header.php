af0ddf222704fbc1eb0879048a400aa936b95aef
Zachary Horine <horinez@oregonstate.edu>
2019-06-02 16:12:13 -0700

    initial review table

--- header.php
+++ header.php
@@ -1,5 +1,19 @@
+<?php
+	include 'connectvars.php';
+
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	if (!$conn) {
+		die('Could not connect: ' . mysql_error());
+	}
+	$account_id = 111111;
+	$sname = mysqli_query($conn, "SELECT `Username` FROM `Account` WHERE Account_ID = $account_id");
+	if (!$sname) {
+		die("Query to show fields from table failed");
+	}
+?>
+
 	<header>
-		OSU Foodie - <em> Welcome <span id="username"><?php echo $_GET["user"];?></span>!</em>
+		OSU Foodie - <em> Welcome <span id="username"><?php echo $sname;?></span>!</em>
 	</header>
 	<nav>
 		<ul>
--- supplierFiles/listParts.php
+++ supplierFiles/listParts.php
@@ -13,40 +13,40 @@
 
 <?php
 // change the value of $dbuser and $dbpass to your username and password
-	include 'connectvars.php'; 
-	include 'header.php';	
+	include 'connectvars.php';
+	include 'header.php';
 
 	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 	if (!$conn) {
 		die('Could not connect: ' . mysql_error());
-	}	
+	}
 
 // query to select all information from supplier table
-	$query = "SELECT * FROM Parts ";
+	$query = "SELECT Username FROM Account WHERE Account_ID = $account_id";
 
-	
+
 // Get results from query
 	$result = mysqli_query($conn, $query);
 	if (!$result) {
 		die("Query to show fields from table failed");
 	}
-// get number of columns in table	
+// get number of columns in table
 	$fields_num = mysqli_num_fields($result);
 	echo "<h1>Parts:</h1>";
 	echo "<table id='t01' border='1'><tr>";
-	
+
 // printing table headers
-	for($i=0; $i<$fields_num; $i++) {	
-		$field = mysqli_fetch_field($result);	
+	for($i=0; $i<$fields_num; $i++) {
+		$field = mysqli_fetch_field($result);
 		echo "<td><b>$field->name</b></td>";
 	}
 	echo "</tr>\n";
-	while($row = mysqli_fetch_row($result)) {	
-		echo "<tr>";	
+	while($row = mysqli_fetch_row($result)) {
+		echo "<tr>";
 		// $row is array... foreach( .. ) puts every element
-		// of $row to $cell variable	
-		foreach($row as $cell)		
-			echo "<td>$cell</td>";	
+		// of $row to $cell variable
+		foreach($row as $cell)
+			echo "<td>$cell</td>";
 		echo "</tr>\n";
 	}
 
@@ -56,5 +56,3 @@
 </body>
 
 </html>
-
-	
