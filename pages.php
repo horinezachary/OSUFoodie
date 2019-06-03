b658fbfdf76f4b2bda563b435251a0f194836937
Michael Zi Huang <huangmic@oregonstate.edu>
2019-06-02 18:22:18 -0700

    Menu attempt

--- pages.php
+++ pages.php
@@ -1,9 +1,10 @@
 <?php
 //$content holds a 2D array with keys being menu names and values being an array with a subtitle, and content
 	$content = array(
-		"Home" => "index.php",
-		"Restaurant" => "restaurant.php",
-		"Account" => "account.php",
-		"About Us" => "aboutUs.php");
+		"Add Supplier" => "addSupplier.php",
+		"Add Part" => "addPart.php",
+		"List Parts" => "listParts.php",
+        "List Suppliers" => "listSuppliers.php",
+        "Menu" => "Menu.php");
 
 ?>
