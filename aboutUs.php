acc45a8bb5f0da314731aa8869dad4e54ef476b4
Michael Zi Huang <huangmic@oregonstate.edu>
2019-06-08 02:49:23 -0700

    Added basic about us

--- aboutUs.php
+++ aboutUs.php
@@ -1,7 +1,37 @@
 <?php include "header.php" ?>
 
         <div>
-          <h2>About Us</h2>
+          <h2>Contact Us</h2>
+        <p class="light"><font color="red"><b>Jack David (color test)</b></font><br>
+        Developer Title (generic) <br>
+        <b>Office Phone</b><br>
+        541-555-4411<br>
+        <b>Email Address</b><br>
+        Jack.David@OSUFoodie.com<br>
+        <br>
+        <b>Zachary Horine</b> <br>
+        Developer Title (generic) <br>
+        <b>Office Phone</b><br>
+        541-555-4262<br>
+        <b>Email Address</b><br>
+        Zachary.Horine@OSUFoodie.com<br>
+        <br>
+        <b>Michael Huang</b> <br>
+        Developer Title (generic) <br>
+        <b>Office Phone</b><br>
+        541-555-4444<br>
+        <b>Email Address</b><br>
+        Michael.Huang@OSUFoodie.com<br>
+        <br>
+        <b>Thomas Korsness</b><br>
+        Developer Title (generic) <br>
+        <b>Office Phone</b><br>
+        541-555-4422<br>
+        <b>Email Address</b><br>
+        Thomas.Korsness@OSUFoodie.com<br>
+        <br>
+        </p>
+
        </div>
     </body>
 </html>
