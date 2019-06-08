2adb8e1bb59b88ef28b80201e6a5cad780b433d6
Zachary Horine <horine.zachary@gmail.com>
2019-06-07 21:09:37 -0700

    Revert pages.php to match master

--- pages.php
+++ pages.php
@@ -1,10 +1,8 @@
 <?php
 //$content holds a 2D array with keys being menu names and values being an array with a subtitle, and content
 	$content = array(
-		"Add Supplier" => "addSupplier.php",
-		"Add Part" => "addPart.php",
-		"List Parts" => "listParts.php",
-        "List Suppliers" => "listSuppliers.php",
-        "Menu" => "Menu.php");
-
+		"Home" => "index.php",
+		"Restaurant" => "restaurant.php",
+		"Account" => "account.php",
+		"About Us" => "aboutUs.php");
 ?>
