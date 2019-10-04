f955adb67febb3f4be412aff7e09b676142a0201
Zachary Horine <horine.zachary@gmail.com>
2019-10-04 21:12:14 +0000

    migrated database and updated image linking on about us page

--- connectvars.php
+++ connectvars.php
@@ -1,8 +1,9 @@
 <?php
   // Define database connection constants
-  define('DB_HOST', 'classmysql.engr.oregonstate.edu');
-  define('DB_USER', 'cs340_horinez');
-  define('DB_PASSWORD', 'BBpF5Cg2LRCuc8F');
+  define('DB_HOST', 'localhost');
+
+  define('DB_USER', 'foodie');
+  define('DB_PASSWORD', 'WmguMrRIH33VbxX8');
   define('DB_NAME', 'cs340_horinez');
-  define('CON_STRING', 'mysql:host=classmysql.engr.oregonstate.edu;dname=cs340_horinez');
+  define('CON_STRING', 'mysql:host=localhost;dname=cs340_horinez');
 ?>
